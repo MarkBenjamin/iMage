df8cc6355f8ffe4d34b0bcd2e0434e6c8f3ec819
SiyanLi <benjaminunknown53@gmail.com>
2020-05-18 00:03:31 +0800

    Finished part f) of Aufgabe4. Finished the method getTileForRegion.

--- iMage.mosaique/src/main/java/org/iMage/mosaique/rectangle/RectangleArtist.java
+++ iMage.mosaique/src/main/java/org/iMage/mosaique/rectangle/RectangleArtist.java
@@ -40,7 +40,19 @@ public class RectangleArtist implements IMosaiqueArtist<BufferedArtImage> {
 
     @Override
     public BufferedArtImage getTileForRegion(BufferedArtImage region) {
-        throw new RuntimeException("not implemented");
+        int[] regionAverageColor = getAverageColor(region);
+        double minDistance = euclideanDistance(regionAverageColor, getAverageColor(tiles.get(0)));
+        BufferedArtImage targetTile = tiles.get(0);
+
+        for (BufferedArtImage tile : this.tiles) {
+            int[] tileColor = getAverageColor(tile);
+            if (euclideanDistance(tileColor, regionAverageColor) < minDistance) {
+                minDistance = euclideanDistance(tileColor, regionAverageColor);
+                targetTile = tile;
+            }
+        }
+        return targetTile;
+
     }
 
     @Override
@@ -51,5 +63,44 @@ public class RectangleArtist implements IMosaiqueArtist<BufferedArtImage> {
     @Override
     public int getTileHeight() {
         throw new RuntimeException("not implemented");
+    }
+
+    public int[] getAverageColor(BufferedArtImage bufferedArtImage) {
+        int height = bufferedArtImage.getHeight();
+        int width = bufferedArtImage.getWidth();
+
+        int sumAlpha = 0;
+        int sumRed = 0;
+        int sumGreen = 0;
+        int sumBlue = 0;
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                sumAlpha += (bufferedArtImage.getRGB(x, y) & 0xff000000) >> 24;
+                sumRed += (bufferedArtImage.getRGB(x, y) & 0x00ff0000) >> 16;
+                sumGreen += (bufferedArtImage.getRGB(x, y) & 0x0000ff00) >> 8;
+                sumBlue += (bufferedArtImage.getRGB(x, y) & 0x000000ff);
+            }
+        }
+
+        int averageAlpha = sumAlpha / (height * width);
+        int averageRed = sumRed / (height * width);
+        int averageGreen = sumGreen / (height * width);
+        int averageBlue = sumBlue / (height * width);
+
+        int[] averageColor = new int[4];
+        averageColor[0] = averageAlpha;
+        averageColor[1] = averageRed;
+        averageColor[2] = averageGreen;
+        averageColor[3] = averageBlue;
+        return averageColor;
+    }
+
+    double euclideanDistance(int[] point1, int[] point2) {
+        double distanceSquare = 0;
+        for (int i = 0; i < point1.length; i++) {
+            distanceSquare += (point1[i] - point2[i]) ^ 2;
+        }
+        return Math.sqrt(distanceSquare);
     }
 }
